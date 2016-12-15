35194089efd5d71e1f11fc2ef14daec8188c24d4
chris <cphang99@gmail.com>
2016-12-15 00:54:01 +0000

    Completition of day 13

--- AOC2016_d13.py
+++ AOC2016_d13.py
@@ -3,6 +3,7 @@ import copy
 fav_number = 1364
 
 INF_COST = 9999
+max_level = 100
 
 def get_element((x, y), fv=fav_number):
     val = str(bin((x*x + 3*x + 2*x*y + y + y*y) + fv))
@@ -33,23 +34,26 @@ def cost_elem(elements, coord, max_dim):
         (coord[0] < 0) or
         (coord[1] < 0)):
             return INF_COST
-    if elements[coord[0]*max_dim + coord[1]] == '.':
-        return 1
-    else:
+    if elements[coord[0]*max_dim + coord[1]] == '#':
         return INF_COST
+    else:
+        return 1
 
-def solve_maze(pos_storage, elements, max_dim, start_pos, end_pos):
+def solve_maze(ps, visited, elements, max_dim, start_pos, end_pos, level=1):
     cost = 0
     mincost = INF_COST
     if cost_elem(elements, end_pos, max_dim) < INF_COST:
         elements[end_pos[0]*max_dim+ end_pos[1]] = 'O'
+        visited.add(end_pos)
     else:
         return INF_COST, elements
 
     if start_pos == end_pos:
-        cost = 0
-    elif end_pos in pos_storage:
-        cost = pos_storage[end_pos]
+        mincost = 0
+    elif end_pos in ps:
+        mincost = ps[end_pos]
+    elif level > max_level:
+        mincost = INF_COST
     else:
         up = (end_pos[0], end_pos[1]-1)
         down = (end_pos[0], end_pos[1]+1)
@@ -59,20 +63,40 @@ def solve_maze(pos_storage, elements, max_dim, start_pos, end_pos):
                             [up, down, left,right])
         costs = []
         for move in pos_moves:
-            costs.append(solve_maze(pos_storage, elements, max_dim, start_pos,
-                move)[0] + cost_elem(elements, move, max_dim))
-        print('from {0} to {1} = {2}'.format(start_pos, end_pos, costs))
+            if move not in visited:
+                v = copy.copy(visited)
+                c, _ = solve_maze(ps, v, elements, max_dim, start_pos, move,
+                        level+1)
+                costs.append(c + cost_elem(elements, move, max_dim))
         if len(costs):
             cost = min(costs)
-            pos_storage[end_pos] = cost
         else:
-            elements[end_pos[0]*max_dim +end_pos[1]] = 'X'
+            cost = INF_COST
+            visited.remove(end_pos)
         if cost < mincost: 
             mincost = cost
+            ps[end_pos] = mincost
     return mincost, elements
 
-pos_storage = {}
-cost, elements = solve_maze(pos_storage, get_maze(50), 50, (1,1), (31,39))
-print_maze(elements, 50)
+def get_locs(within_dist, max_dim, start_pos):
+    num_locs = 0
+    elements = get_maze(max_dim)
+    l = [c for c in xrange(max_dim)]
+    coordinates = [c for c in itertools.product(l, repeat=2)]
+    pos_storage = {}
+    for coord in coordinates:
+        cost, _ = solve_maze(pos_storage, set(), elements, max_dim, 
+                            start_pos, coord)
+        if cost <= within_dist:
+            num_locs += 1
+            print(num_locs)
+            print(coord)
+    return num_locs
+
+max_dim = 50
+cost, elements = solve_maze(dict(), set(), get_maze(max_dim), max_dim,
+        (1,1),(31,39))
+print_maze(elements, max_dim)
 print(cost)
+print(get_locs(50, 50, (1,1)))
 
